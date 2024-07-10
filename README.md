8553770d8eb5ae555e86795f9407a3c20635b5bd
Yancy Garret <yancygarret12@gmail.com>
2024-07-10 18:31:51 +0800

    starting point

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my furniro ecommerce application version 3
+
+TO DO:
+[] - I am gonna use nextjs
+[] - Refactored the forms using shadcn
---
+++ public/images/index.ts
@@ -0,0 +1,49 @@
+import inspiration1 from "../images/inspiration/inspiration1.jpg";
+import inspiration2 from "../images/inspiration/inspiration2.jpg";
+import inspiration3 from "../images/inspiration/inspiration3.jpg";
+import inspiration4 from "../images/inspiration/inspiration4.jpg";
+import dining from "../images/categories/dining.jpg";
+import living from "../images/categories/living.jpg";
+import bedroom from "../images/categories/bedroom.jpg";
+import kitchen from "../images/categories/dining.jpg";
+import workplace from "../images/categories/workplace.jpg";
+import cart from "../images/header/cart.png";
+import cartClose from "../images/sidebarCart/cart-close.png";
+import deleteIcon from "../images/sidebarCart/item-delete.png";
+import extendedHeaderImage from "../images/header/extendedHeader.jpg";
+import logo from "../images/header/logo.png";
+import furniro from "../images/header/furniro.png";
+import guarantee from "../images/features/guarantee.png";
+import shipping from "../images/features/shipping.png";
+import support from "../images/features/support.png";
+import trophy from "../images/features/trophy.png";
+import phone from "../images/contact/phone.png";
+import clock from "../images/contact/clock.png";
+import location from "../images/contact/location.png";
+import trashcan from "../images/sidebarCart/trashcan.png";
+
+export {
+  inspiration1,
+  inspiration2,
+  inspiration3,
+  inspiration4,
+  dining,
+  living,
+  bedroom,
+  kitchen,
+  workplace,
+  logo,
+  cart,
+  extendedHeaderImage,
+  furniro,
+  guarantee,
+  shipping,
+  support,
+  trophy,
+  phone,
+  location,
+  clock,
+  cartClose,
+  deleteIcon,
+  trashcan,
+};

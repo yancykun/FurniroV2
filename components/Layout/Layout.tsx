a3242c49717a9748ff6813ebd480895a51000e65
Yancy Garret <yancygarret12@gmail.com>
2024-07-12 21:40:05 +0800

    refactored layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,29 @@
+'use client';
+
 import { ReactNode } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SidebarOverlay from '../SidebarOverlay/SidebarOverlay';
 import CartSidebarComponent from '../CartSidebar/CartSidebarComponent';
+import { usePathname } from 'next/navigation';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+    const pathname = usePathname();
+    const isAuthPage = pathname === '/signup';
+
     return (
         <>
-            <Header />
-            {children}
-            <Footer />
-            <SidebarOverlay />
-            <CartSidebarComponent />
+            {isAuthPage ? (
+                children
+            ) : (
+                <>
+                    <Header />
+                    {children}
+                    <Footer />
+                    <SidebarOverlay />
+                    <CartSidebarComponent />
+                </>
+            )}
         </>
     );
 };
